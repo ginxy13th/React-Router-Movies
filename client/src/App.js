24686c0bbf81f5ae3abc23ec4cc49b18e8a2d6a9
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-20 14:17:40 -0400

    added route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Switch, Link, Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -28,7 +30,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Route path='/'>
+          <MovieList  />
+        </Route>
+
+        <Route path='/movies/:movieId'>
+          <Movie  />
+        </Route>
     </div>
   );
 };
